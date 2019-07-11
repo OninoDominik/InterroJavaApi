d745b8a99940a10e50211e90086c5a889c4257af
OninoDominik <grobecdo@gmail.com>
2019-07-11 09:09:26 +0200

    Initializer commit

---
+++ InterroJavaApi/src/main/java/com/example/InterroJavaApi/InterroJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.InterroJavaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterroJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterroJavaApiApplication.class, args);
+	}
+
+}
